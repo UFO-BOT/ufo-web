793b56025d02e24072c2d452c601b42df006f5f7
SmokyPlay <smokyplay.kr@gmail.com>
2021-03-27 00:14:52 +0300

    Add categories settings

---
+++ src/components/Category.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn icon v-bind="attrs" v-on="on">
+          <v-icon>settings</v-icon>
+        </v-btn>
+      </template>
+      <v-card>
+        <v-toolbar dark color="primary">
+          <v-btn icon dark :disabled="loading" @click="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-toolbar-title>{{ category }}</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn dark text :disabled="!valid" :loading="loading" @click="editCategory">
+              <v-icon medium class="save-icon">save</v-icon>
+              {{ content.submit }}
+            </v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        <div class="command-settings">
+          <v-form ref="form" v-model="valid">
+            <div class="subtitle">{{ content.subtitles.general }}</div>
+            <v-switch v-model="settings.enabled" :label="content.subtitles.enabled" class="fit-content mt-1"></v-switch>
+            <v-checkbox v-model="settings.deleteUsage" :disabled="!settings.enabled" :label="content.subtitles.deleteUsage" class="fit-content ma-0"></v-checkbox>
+            <div class="subtitle">{{ content.subtitles.perms }}</div>
+            <v-select
+                v-for="perm of permsList"
+                v-model="settings[perm]"
+                class="perms-select mb-2"
+                multiple chips deletable-chips
+                :label="content.subtitles[perm].name"
+                :hint="content.subtitles[perm].description"
+                persistent-hint
+                :disabled="!settings.enabled"
+                :items="perm.toLowerCase().includes('roles') ? roles : channels">
+            </v-select>
+          </v-form>
+        </div>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import WebContent from '@/content.json'
+import Cookies from '@/util/cookies'
+import config from "@/config.json";
+import ParseForSelect from "@/util/parseForSelect";
+
+let cookies = Cookies.parse()
+let content = WebContent.GuildCommands[cookies.language]
+
+export default {
+  name: "Category",
+  props: ['name', 'category'],
+  data: () => ({
+    content,
+    loading: false,
+    dialog: false,
+    valid: true,
+    settings: {
+      enabled: true,
+      deleteUsage: false,
+      allowedRoles: [],
+      forbiddenRoles: [],
+      allowedChannels: [],
+      forbiddenChannels: []
+    },
+    permsList: ['allowedRoles', 'forbiddenRoles', 'allowedChannels', 'forbiddenChannels']
+  }),
+  computed: {
+    roles() {
+      let guild = this.$store.getters.guilds.find(g => g.id === this.$route.params.id)
+      if (!guild) return [];
+      return ParseForSelect.roles(guild.roles, {checkBotManageable: false})
+    },
+    channels() {
+      let guild = this.$store.getters.guilds.find(g => g.id === this.$route.params.id)
+      if(!guild) return [];
+      return ParseForSelect.channels(guild.channels, {none: false})
+    }
+  },
+  methods: {
+    async editCategory() {
+      this.loading = true;
+      let response = await fetch(`${config.API}/private/guild/${this.$route.params.id}/categories/${this.name}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: cookies.token
+        },
+        body: JSON.stringify(this.settings)
+      })
+      if(response.ok) {
+        this.dialog = false;
+        this.$emit('updated', this.name, this.settings)
+      }
+      this.loading = false;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.subtitle {
+  font-size: 1.2em;
+}
+
+.command-settings {
+  padding: 15px;
+}
+
+.perms-select {
+  width: 95%;
+}
+
+.save-icon {
+  margin-right: 5px;
+}
+</style>
--- src/views/GuildSettings/Commands.vue
+++ src/views/GuildSettings/Commands.vue
@@ -5,18 +5,24 @@
                            indeterminate></v-progress-circular>
     </div>
     <v-form ref="form" v-if="!loading">
-      <v-expansion-panels class="commands" accordion focusable>
+      <v-expansion-panels class="commands" accordion :focusable="!categorySettingsHover">
         <v-expansion-panel
             v-for="(category, i) of categories"
             :key="i"
+            :readonly="categorySettingsHover"
         >
-          <v-expansion-panel-header class="category" ripple v-if="commands.find(c => c.category.en === category)"><span><v-icon large left>{{ icons[i] }}</v-icon>{{commands.find(c => c.category.en === category).category[language]}}</span></v-expansion-panel-header>
+          <v-expansion-panel-header class="category" :ripple="!categorySettingsHover" v-if="commands.find(c => c.category.en === category)"><span><v-icon large left>{{ icons[i] }}</v-icon>{{commands.find(c => c.category.en === category).category[language]}}</span>
+            <template v-slot:actions>
+              <Category :category="commands.find(c => c.category.en === category).category[language]" :name="category" @updated="categoryUpdated"></Category>
+              <v-icon>$expand</v-icon>
+            </template>
+          </v-expansion-panel-header>
           <v-expansion-panel-content>
             <div v-for="(cmd, i) of commands.filter(c => c.category.en === category)">
               <div class="command">
                 <div class="cmd-name">{{settings.prefix + cmd.name[settings.language.commands]}}</div>
                 <div style="float: right">
-                  <Command :command="settings.prefix + cmd.name[settings.language.commands]" :name="cmd.name.en" :settings="settings.commands[cmd.name.en] || defaultCommand"></Command>
+                  <Command :command="settings.prefix + cmd.name[settings.language.commands]" :name="cmd.name.en" :settings="settings.commands[cmd.name.en]"></Command>
                 </div>
               </div>
               <v-divider v-if="i < commands.filter(c => c.category.en === category).length-1"></v-divider>
@@ -34,6 +40,7 @@ import Cookies from '@/util/cookies'
 import config from "@/config.json";
 
 import Command from "@/components/Command";
+import Category from "@/components/Category";
 
 let cookies = Cookies.parse()
 let content = WebContent.GuildCommands[cookies.language]
@@ -41,6 +48,7 @@ let content = WebContent.GuildCommands[cookies.language]
 export default {
   name: 'Commands',
   components: {
+    Category,
     Command
   },
   metaInfo: {
@@ -56,19 +64,25 @@ export default {
     rules: {
     },
     commands: [],
-    defaultCommand: {
-      enabled: true,
-      deleteUsage: false,
-      allowedRoles: [],
-      forbiddenRoles: [],
-      allowedChannels: [],
-      forbiddenChannels: []
-    },
     categories: ['General', 'Economy', 'Games', 'Utilities', 'Moderation'],
     icons: ['public', 'attach_money', 'phone_iphone', 'build', 'security'],
+    categorySettingsHover: false,
     submitting: false
   }),
   methods: {
+    categoryUpdated(category, settings) {
+      this.commands.filter(cmd => cmd.category.en === category).forEach(cmd => {
+        this.settings.commands[cmd.name.en] = {
+          name: cmd.name.en,
+          enabled: settings.enabled,
+          deleteUsage: settings.deleteUsage,
+          allowedRoles: settings.allowedRoles,
+          forbiddenRoles: settings.forbiddenRoles,
+          allowedChannels: settings.allowedChannels,
+          forbiddenChannels: settings.forbiddenChannels
+        };
+      })
+    }
   },
   async mounted() {
     let commands = await fetch(`${config.API}/public/commands`)
@@ -79,6 +93,7 @@ export default {
     let body = await response.json()
     if(!response.ok) return window.location.replace('/@me');
     this.settings = body;
+    console.log(this.settings.commands)
     this.loading = false;
   }
 }
