feeedef9e4437fcff6be0000ef4d04737ffe098d
SmokyPlay <smokyplay.kr@gmail.com>
2021-05-09 16:23:53 +0300

    Update logs

--- src/views/GuildSettings/Logs.vue
+++ src/views/GuildSettings/Logs.vue
@@ -6,9 +6,35 @@
     </div>
     <v-form ref="form" v-model="valid" v-if="!loading">
       <div class="subtitle">{{ content.subtitles.logs }}</div>
-      <v-select v-model="settings.deletelog" :items="channels" :label="content.subtitles.deletelog" class="channel-select"></v-select>
-      <v-select v-model="settings.editlog" :items="channels" :label="content.subtitles.editlog" class="channel-select"></v-select>
-      <v-select v-model="settings.modlog" :items="channels" :label="content.subtitles.modlog" class="channel-select"></v-select>
+      <v-expansion-panels class="logs mt-1" accordion hover focusable>
+        <v-expansion-panel v-for="(category, i) of categories" :key="i">
+          <v-expansion-panel-header class="category" color="block" ripple>
+            <span>
+              <v-icon large left>{{ content.subtitles.categories[category].icon }}</v-icon>
+              {{ content.subtitles.categories[category].name }}
+            </span>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content color="block">
+            <div v-for="(log, j) of logs[category]">
+              <div class="log" v-if="settings.logs.list[log]">
+                <div class="log-name">{{ content.subtitles.logsList[log] }}</div>
+                <div class="log-settings">
+                  <v-select v-model="settings.logs.list[log].channel" :disabled="!settings.logs.list[log].enabled" :items="channels" :label="content.subtitles.channel" hide-details class="channel-select mr-3"/>
+                  <v-switch v-model="settings.logs.list[log].enabled" class="switch"/>
+                </div>
+              </div>
+              <v-divider v-if="j < logs[category].length-1"></v-divider>
+            </div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+      <div class="subtitle mt-3">{{ content.subtitles.ignoredChannels }}</div>
+      <v-select v-model="settings.logs.ignore.channels"
+                :label="content.subtitles.channels"
+                :hint="content.subtitles.ignoredChannelsHint"
+                persistent-hint multiple chips deletable-chips
+                :items="channels"
+                class="channels-select mb-2"/>
       <v-btn :disabled="!valid" :loading="submitting" large color="secondary" class="submit" @click="submit"><v-icon medium class="save-icon">save</v-icon>{{ content.submit }}</v-btn>
     </v-form>
     <v-snackbar v-model="result" color="secondary">
@@ -36,10 +62,20 @@ export default {
     content,
     loading: true,
     valid: true,
+    categories: ['messages', 'roles', 'moderation'],
+    logs: {
+      messages: ['messageDelete', 'messageEdit', 'messageDeleteBulk'],
+      roles: ['roleCreate', 'roleEdit', 'roleDelete'],
+      moderation: ['moderationWarn', 'moderationMute', 'moderationKick', 'moderationBan',
+      'moderationUnmute', 'moderationUnban']
+    },
     settings: {
-      deletelog: 'none',
-      editlog: 'none',
-      modlog: 'none'
+      logs: {
+        list: {}
+      },
+      ignore: {
+        channels: []
+      }
     },
     submitting: false,
     result: null,
@@ -50,7 +86,7 @@ export default {
     channels() {
       let guild = this.$store.getters.guilds.find(g => g.id === this.$route.params.id)
       if(!guild) return [];
-      return ParseForSelect.channels(guild.channels)
+      return ParseForSelect.channels(guild.channels, {none: false})
     }
   },
   methods: {
@@ -59,11 +95,7 @@ export default {
       let response = await fetch(`${config.API}/private/guild/${this.$route.params.id}/logs`, {method: 'POST', headers: {
           Authorization: localStorage.getItem('token'),
           'Content-Type': 'application/json'
-        }, body: JSON.stringify({
-          deletelog: this.settings.deletelog,
-          editlog: this.settings.editlog,
-          modlog: this.settings.modlog
-        })})
+        }, body: JSON.stringify(this.settings.logs)})
       if(response.ok) {
         this.error = false;
         this.resultText = content.submitted;
@@ -93,8 +125,35 @@ export default {
 .subtitle {
   font-size: 1.2em;
 }
+.channels-select {
+  width: 95%;
+}
 .channel-select {
-  width: 250px;
+  max-width: 229px;
+  display: inline-block;
+}
+.switch {
+  display: inline-block;
+}
+.category {
+  font-size:  1.5em;
+  padding: 26px 26px!important;
+}
+.logs {
+  width: 95%;
+}
+.log {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 1.2em;
+  margin-top: 4px;
+  word-break: break-all;
+}
+.log-name {
+  font-size: 1em;
 }
 .save-icon {
   margin-right: 5px;
